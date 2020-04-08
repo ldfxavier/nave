69dab07d9eeb6822d3c7b10d194bf20d929d09ac
root <root@DESKTOP-U1DFHHI.localdomain>
2020-04-08 12:20:01 -0300

    corrigido link do login

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
             <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contato</a>
           </li>
           <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/nave/login" target="_blank">Login</a>
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/login" target="_blank">Login</a>
           </li>
         </ul>
       </div>
